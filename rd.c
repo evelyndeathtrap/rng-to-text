a3bec53f3dfe2ae55ba9feeb03102d45ae1a0ea6
evelyndeathtrap <evelyn.death.trap@gmail.com>
2026-02-28 18:28:07 +0200

    Update rd.c

--- rd.c
+++ rd.c
@@ -1,25 +1,30 @@
+  GNU nano 7.2              rd.c                       
 #include <stdio.h>
 #include <immintrin.h>
 #include <unistd.h>
 #include "words.h"
 int main() {
-    // _rdrand32_step returns 1 on success, 0 on failure
+    // _rdrand32_step returns 1 on success, 0 on failu>
     unsigned short a;
-    unsigned short b;
     while(5) {
 
     _rdrand16_step(&a);
+    unsigned long long count = 0;
+    while(lines[count] != NULL) {
+        count++;
+    }    
 //    _rdrand16_step(&b);
    // unsigned long long r = b - a;
-	if (a >= line_count)
-{ b; a; }		
-	else
-	printf("%s ", lines[(unsigned long long)~a % (line_count-1)]);
-	fflush(stdout);
-	
-	}
+        if (a >= count)
+{ a; }          
+        else
+        printf("%s ", lines[(unsigned long long)a);
+        fflush(stdout);
+
+        }
 
    
     
     return 0;
 }
+
